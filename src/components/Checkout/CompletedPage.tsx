d22f567435c37635c758593ba3a4dc5e158f4888
patrikpecenka <ppecenka@icloud.com>
2024-03-21 10:24:54 +0100

    feat: new page with thanks for purchase and removed framer motion

---
+++ src/components/Checkout/CompletedPage.tsx
@@ -0,0 +1,38 @@
+import { Button, Card, Flex, Group, Title, Text } from "@mantine/core";
+import { Link } from "react-router-dom";
+
+export const CompletedPage = () => {
+  return (
+    <>
+      <Card
+        shadow="sm"
+        h={600}
+      >
+        <Flex align="center" justify="center" direction="column" h="100%" gap={15}>
+          <Title order={1}>
+            Thank you for your purchase!
+          </Title>
+          <Text c="dimmed">
+            Now you can close the page or continue shopping
+          </Text>
+          <Group>
+            <Button
+              component={Link}
+              to={"/profile"}
+              variant="light"
+            >
+              Check orders
+            </Button>
+            <Button
+              component={Link}
+              to={"/products"}
+              variant="gradient"
+            >
+              Done
+            </Button>
+          </Group>
+        </Flex>
+      </Card>
+    </>
+  );
+};
--- src/pages/CheckoutPage.tsx
+++ src/pages/CheckoutPage.tsx
@@ -1,75 +1,61 @@
-import { Button, Card, Stepper, Text } from "@mantine/core"
-import { OrderOne } from "../components/Checkout/OrderOne"
-import { OrderTwo } from "../components/Checkout/OrderTwo"
-import { OrderThree } from "../components/Checkout/OrderThree"
-import { NumberParam, useQueryParam, withDefault } from "use-query-params"
-import { useCart, useOrderCart } from "store/shopStore"
-import { useEffect } from "react"
-import { IconLock } from "@tabler/icons-react"
-import { useNavigate } from "react-router-dom"
-import { motion } from "framer-motion";
+import { Card, Stepper } from "@mantine/core";
+import { CartSummary } from "../components/Checkout/CartSummary";
+import { PaymentDelivery } from "../components/Checkout/PaymentDelivery";
+import { Address } from "../components/Checkout/Address";
+import { NumberParam, useQueryParam, withDefault } from "use-query-params";
+import { useCartStore } from "store/cart.store";
+import { useEffect } from "react";
+import { IconLock } from "@tabler/icons-react";
+import { useNavigate } from "react-router-dom";
+import { CompletedPage } from "components/Checkout/CompletedPage";
 
 export const CheckoutPage = () => {
-  const { cart } = useCart()
-  const { userData } = useOrderCart()
-  const navigate = useNavigate()
+  const { cart } = useCartStore();
+  const navigate = useNavigate();
 
   const [checkoutStep, setCheckoutStep] = useQueryParam(
     "checkoutStep", withDefault(NumberParam, 0),
   );
 
-  // useEffect(() => {
-  //   cart.length === 0 && checkoutStep >= 1
-  //     ? navigate("/checkout?checkoutStep=0")
-  //     : null
-  // }, [cart.length, checkoutStep])
+  useEffect(() => {
+    cart.length === 0 && checkoutStep < 3
+      ? navigate("/checkout?checkoutStep=0")
+      : null;
+  }, [cart.length, checkoutStep]);
 
   return (
-    <motion.div
-      key="checkout-page"
-      initial={{ x: "20%", opacity: 0 }}
-      animate={{ x: 0, opacity: 1 }}
-      exit={{ x: "-20%", opacity: 0, transition: { duration: 0.2 } }}
-      transition={{ delay: 0, duration: 0.2 }}
+    <Card
+      h="100%"
+      mx="10rem"
+      mt={10}
     >
-      <Card
-        h="100%"
-        mx="10rem"
-        mt={10}
-      >
-        <Stepper active={checkoutStep} color="indigo" size="sm" onStepClick={setCheckoutStep} allowNextStepsSelect={cart.length === 0 ? false : true}>
-          <Stepper.Step label="Cart" >
-            <OrderOne handleStepForward={() => setCheckoutStep(checkoutStep + 1)} />
-          </Stepper.Step>
-          <Stepper.Step
-            label="Payment Delivery"
-            disabled={cart.length === 0}
-            icon={cart.length === 0 ? <IconLock /> : undefined}
-          >
-            <OrderTwo
-              handleStepBackwards={() => setCheckoutStep(checkoutStep - 1)}
-              handleStepForward={() => setCheckoutStep(checkoutStep + 1)}
-            />
-          </Stepper.Step>
-          <Stepper.Step
-            label="Address"
-            icon={cart.length === 0 ? <IconLock /> : undefined}
-            allowStepSelect={false}
-          >
-            <OrderThree handleStepCompleted={() => setCheckoutStep(3)} />
-          </Stepper.Step>
-          <Stepper.Completed >
-            <Text>Here is ur invoice</Text>
-            {/* <PDFDownloadLink document={<PdfFile />} fileName={`${userData.map((item) => item.id)[0]}.pdf`}>
-            {({ loading }) =>
-              loading
-                ? <Button> Loading document.... </Button>
-                : <Button>Download invoice</Button>
-            } 
-           </PDFDownloadLink> */}
-          </Stepper.Completed>
-        </Stepper>
-      </Card>
-    </motion.div>
-  )
-}
+      <Stepper active={checkoutStep} color="indigo" size="sm" onStepClick={setCheckoutStep} allowNextStepsSelect={cart.length === 0 ? false : true}>
+        <Stepper.Step label="Cart" >
+          <CartSummary handleStepForward={() => setCheckoutStep(checkoutStep + 1)} />
+        </Stepper.Step>
+        <Stepper.Step
+          label="Payment Delivery"
+          disabled={cart.length === 0}
+          icon={cart.length === 0 ? <IconLock /> : undefined}
+        >
+          <PaymentDelivery
+            handleStepBackwards={() => setCheckoutStep(checkoutStep - 1)}
+            handleStepForward={() => setCheckoutStep(checkoutStep + 1)}
+          />
+        </Stepper.Step>
+        <Stepper.Step
+          label="Address"
+          icon={cart.length === 0 ? <IconLock /> : undefined}
+          allowStepSelect={false}
+        >
+          <Address handleStepCompleted={() => setCheckoutStep(3)} />
+        </Stepper.Step>
+        <Stepper.Completed >
+          <CompletedPage />
+
+        </Stepper.Completed>
+      </Stepper>
+    </Card>
+
+  );
+};
