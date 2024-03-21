c6bfff00eebf8dfa45dfb1b83af48aa19b3f2b72
patrikpecenka <ppecenka@icloud.com>
2024-03-21 10:36:39 +0100

    style: theme.color remove from main, feat: remove framer motion from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,28 @@
-import TempLayout from "layouts/AppShell"
-import { ProductsListPage } from "pages/ProductsListPage"
-import { SoloProductPage } from "pages/SoloProductPage"
-import { Route, Routes, Navigate, useLocation } from "react-router-dom"
-import { CheckoutPage } from "pages/CheckoutPage"
-import { ProfilePage } from "pages/OrderDetailPage"
-import { AnimatePresence } from "framer-motion"
-
-
+import { AppShellLayout } from "layouts/AppShellLayout";
+import { ProductsListPage } from "pages/ProductsListPage";
+import { SoloProductPage } from "pages/ProductDetailPage";
+import { Route, Routes, Navigate, useLocation } from "react-router-dom";
+import { CheckoutPage } from "pages/CheckoutPage";
+import { ProfilePage } from "pages/OrderDetailPage";
 const App = () => {
-  const location = useLocation()
+  const location = useLocation();
 
   return (
-    <AnimatePresence mode="wait" initial={false} onExitComplete={() => window.scrollTo(0, 0)}>
-      <Routes location={location} key={location.pathname}>
-        <Route element={<TempLayout />}>
-          <Route path="/products" element={<ProductsListPage />} />
-          <Route path="/products/:id/" element={<SoloProductPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/checkout" element={<CheckoutPage />} />
+    <Routes location={location} key={location.pathname}>
+      <Route element={<AppShellLayout />}>
+        <Route path="/products" element={<ProductsListPage />} />
+        <Route path="/products/:id/" element={<SoloProductPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
 
+        <Route element={<ProfilePage />} >
+          <Route path="/profile/" element={<ProfilePage />} />
+        </Route >
 
-          <Route path="*" element={<Navigate to="/products" replace />} />
-        </Route>
-      </Routes>
-    </AnimatePresence >
-  )
-}
+        <Route path="*" element={<Navigate to="/products" replace />} />
+      </Route>
+    </Routes>
 
-export default App
+  );
+};
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -3,32 +3,30 @@ import '@mantine/core/styles.css';
 import '@mantine/carousel/styles.css';
 import '@mantine/notifications/styles.css';
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from '../src/App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from '../src/App';
 import { QueryParamProvider } from 'use-query-params';
 import { ReactRouter6Adapter } from 'use-query-params/adapters/react-router-6';
 import { ModalsProvider } from '@mantine/modals';
 import { MantineProvider, createTheme } from '@mantine/core';
 import { BrowserRouter } from 'react-router-dom';
 import { Notifications } from '@mantine/notifications';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
 
 const theme = createTheme({
-  colors: {
-  },
   defaultGradient: {
-    from: 'violet',
-    to: 'indigo',
-    deg: 25,
+    from: "violet",
+    to: "indigo",
+    deg: 25
   }
-})
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={queryClient} >
       <MantineProvider theme={theme}>
         <ModalsProvider >
           <BrowserRouter >
@@ -41,4 +39,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       </MantineProvider>
     </QueryClientProvider>
   </React.StrictMode>,
-)
+);
